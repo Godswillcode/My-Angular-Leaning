528567243c8142c5653390b2ff8896536957320c
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-02-17 12:35:22 +0100

    forms

--- src/app/form-td/form-td.component.ts
+++ src/app/form-td/form-td.component.ts
@@ -1,12 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-    selector: 'app-form-td',
-    templateUrl: './form-td.component.html'
+  selector: 'app-form-td',
+  templateUrl: './form-td.component.html',
+  styleUrls: ['./form-td.component.css']
 })
-
 export class FormTdComponent {
-    suggestUserName() {
-        const suggestedName = 'Superuser';
-      }
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet'
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  //   onSubmit(form: NgForm) {
+  //     console.log(form);
+  //   }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
 }
